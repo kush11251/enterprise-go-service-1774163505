a62a0915157aa08293536923cb89c5ebf1f2b983
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-22 12:41:53 +0530

    feat: integrate pkg/models/user.go

---
+++ pkg/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+    ID       int    `json:"id"`
+    Username string `json:"username"`
+    Email    string `json:"email"`
+    Active   bool   `json:"active"`
+}
